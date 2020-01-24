cb9aede41eee3de8dc4f9b48f769b56a8fd8bbff
alokvrm02 <alokvrm02@gmail.com>
2020-01-25 02:20:22 +0530

    first

---
+++ app/src/main/java/com/shivenderkumar/groupchatapp/Constants.java
@@ -0,0 +1,7 @@
+package com.shivenderkumar.groupchatapp;
+
+public class Constants {
+
+    public static final String CHAT_SERVER_URL = "https://192.168.0.112:8080/chat";
+
+}
---
+++ app/src/main/java/com/shivenderkumar/groupchatapp/GroupChatApp.java
@@ -0,0 +1,25 @@
+package com.shivenderkumar.groupchatapp;
+
+import android.app.Application;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class GroupChatApp extends Application {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket(Constants.CHAT_SERVER_URL);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Socket getSocket() {
+        return mSocket;
+    }
+
+}
